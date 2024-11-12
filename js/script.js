187d549c42d71bddc02ef1a7c3240457249f4aa4
Afsana-fazil <afsanafazil03@gmail.com>
2024-11-12 12:47:40 +0530

    added responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// for filtering object
+
 document.querySelector('#searchInput')
         .addEventListener('input', filterList);
 
@@ -15,5 +17,49 @@ document.querySelector('#searchInput')
             }
         });
     }
+
+
+
+//for toggle button
+
+const toggleButtons = document.getElementsByClassName("toggle");
+
+for (let i = 0; i < toggleButtons.length; i++) {
+    toggleButtons[i].addEventListener("click", function() {
+        const content = document.querySelector("#content .content_box");
+        
+        if (content.style.display === "none" || content.style.display === "") {
+            content.style.display = "block"; 
+        } else {
+            content.style.display = "none"; 
+        }
+    });
+}
+
+    
+
+// Toggle the visibility
+
+function toggleSearchInput() {
+    var searchInput = document.getElementById('searchInput');
+    searchInput.classList.toggle('show'); 
+    searchInput.placeholder = "Search Product...";
+}
+
+
+
+
+// toggle the menu
+
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+
 
 
